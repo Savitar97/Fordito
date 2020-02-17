98a8927c3392a740fc472fb625113bc6cc9fe140
Attila Tóth <43468256+Savitar97@users.noreply.github.com>
2020-02-17 14:00:24 +0100

    Add files via upload

--- Fordito.java
+++ Fordito.java
@@ -1,74 +1,103 @@
-import java.util.HashMap;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+
 public class Fordito {
+    private final String[] abc;
+    private final int[][] allapotatmenet;
+    private final int vegallapot;
+    private int state;
+    private int kezdoallapot;
 
-    static HashMap<String,String> Lexical =new HashMap<String, String>();
+    Fordito(String[] abc, int[][] allapotatmenet, int kezdoallapot, int vegallapot) {
+        this.abc = abc;
+        this.allapotatmenet = allapotatmenet;
+        this.kezdoallapot = kezdoallapot;
+        this.vegallapot = vegallapot;
+        System.out.println("elindult");
+    }
 
-    static void lexicalSearch(String a){
-        char[] Operators={'%','+','-','>','<'};
-        for (int i = 0; i < a.length(); i++) {
-            for (int j = 0; j < Operators.length; j++) {
-                if (a.charAt(i) == Operators[j]) {
-                    String temp = String.valueOf(a.charAt(i));
-                    Lexical.put(temp, "Operátor");
+    boolean isValid(String s) {
+        state = kezdoallapot;
+        int test;
+
+        for (int i = 0; i < s.length(); i++) {
+
+                String Type = "other";
+
+                    if (Character.isDigit(s.charAt(i))) {
+                        Type = "number";
+                        System.out.println(Type);
+                    } else if (Character.isAlphabetic(s.charAt(i))) {
+                        Type = "letter";
+                        System.out.println(Type);
+                    } else if (s.charAt(i) == '{') {
+                        Type = "{";
+                        System.out.println(Type);
+                    } else if (s.charAt(i) == '{') {
+                        Type = "}";
+                        System.out.println(Type);
+                    } else if (s.charAt(i) == '(') {
+                        Type = "(";
+                        System.out.println(Type);
+                    } else if (s.charAt(i) == '*') {
+                        Type = "*";
+                        System.out.println(Type);
+                    } else if (s.charAt(i) == ')') {
+                        Type = ")";
+                        System.out.println(Type);
+                    } else if (s.charAt(i) == ':') {
+                        Type = ":";
+                        System.out.println(Type);
+                    } else if (s.charAt(i) == '=') {
+                        Type = "=";
+                        System.out.println(Type);
+                    } else if (s.charAt(i) == '<') {
+                        Type = "<";
+                        System.out.println(Type);
+                    } else if (s.charAt(i) == '>') {
+                        Type = ">";
+                        System.out.println(Type);
+                    } else if (s.charAt(i) == ' ') {
+                        Type = "space";
+                        System.out.println(Type);
+                    } else if (s.charAt(i) == '$') {
+                        Type = "$";
+                        System.out.println(Type);
+                    }
+
+                    test = findState(Type);
+                    state = allapotatmenet[state][test];
+                    System.out.println(state);
+                    if (state == 0) {
+                        return false;
+                    }
+                    if (state == 21) {
+                        return true;
+                    }
                 }
-                if (a.charAt(i) == '=' && a.charAt(i - 1) == ':') {
-                    Lexical.put(":=", "Token");
-                }
-                if (a.charAt(i) == '>' && a.charAt(i - 1) == '<') {
-                    Lexical.put("<>", "Operátor");
-                }
-                if (a.charAt(i)=='/'&&(a.charAt(i-1)!='/'&&a.charAt(i+1)!='/')) {
-                    Lexical.put("/", "Operátor");
-                }
-                if(a.charAt(i)=='*'&&(a.charAt(i-1)!='('&&a.charAt(i+1)!=')')){
-                        Lexical.put("*", "Operátor");
-                }
 
-
-
-            }
-            }
-
-        }
-
-
-
-    public static void main(String[] args) {
-
-        System.out.println("Add meg a kifejezést:");
-        String kif = System.console().readLine();
-        lexicalSearch(kif);
-        String pattern1 = "^[^\\w](\\d)+((.)(\\d)+)?";
-        String pattern2="[a-zA-Z]+[1-9]*";
-        String pattern3="(\\/\\/)(\\w)*(\\d)*";
-        String pattern4= "(\\(\\*)[\\d\\w\\W\\D\\s\\S]*(\\*\\))";
-        Pattern r = Pattern.compile(pattern1);
-        Matcher m = r.matcher(kif);
-        if(m.find()){
-
-            Lexical.put(m.group(),"Szám");
-        }
-        r = Pattern.compile(pattern2);
-        m = r.matcher(kif);
-        while(m.find()){
-            Lexical.put(m.group(),"Változó");
-
-        }
-        r = Pattern.compile(pattern3);
-        m = r.matcher(kif);
-        while(m.find()){
-            Lexical.put(m.group(),"Egysoros komment");
-        }
-        r = Pattern.compile(pattern4);
-        m = r.matcher(kif);
-        while(m.find()){
-            Lexical.put(m.group(),"Többsoros komment");
-        }
-        for (String i:Lexical.keySet()) {
-            System.out.println(i+' '+Lexical.get(i));
-        }
+        return elfogadoAllapot();
 
     }
+
+    int findState(String s) {
+
+        for (int i = 0; i < abc.length; i++) {
+            if (s.equals(abc[i])) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+        boolean elfogadoAllapot ()
+        {
+
+            if (state == vegallapot) {
+                return true;
+            } else {
+                return false;
+            }
+
+        }
+
 }
+
